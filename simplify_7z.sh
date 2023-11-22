c0ea4efe62abdb5e3217d0f047bbd26e6a7d63e9
wingxel <wingxel@gmail.com>
2023-11-23 00:03:36 +0300

    Simplify 7z with a script

---
+++ simplify_7z.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ];
+then
+	echo "Ivalid arguments"
+	echo "Usage: protectData destination.7z sourceFolder"
+	exit 1
+fi
+
+if [ -e $1 ];
+then
+	echo "Destination file $1 already exists"
+	exit 1
+fi
+
+if [ ! -f $2 ] && [ ! -d $2 ];
+then
+	echo "Source file $2 does not exist"
+	exit 1
+fi
+
+if [[ ! $1 =~ \.7z$ ]];
+then
+	echo "Destination file should have .7z extension (destFile.7z)"
+	exit 1
+fi
+
+7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mhe=on -p $1 $2
